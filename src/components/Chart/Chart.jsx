cb4f89b311ff2cebff35e820d84d1c10a7476574
Nanci <sikabarca@gmail.com>
2020-04-23 01:58:16 -0500

    Add bar chart for specific country

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-const Charts = () => {
+const Charts = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setDailyData] = useState([]);
     useEffect(() => {
         const fetchAPI = async () => {
@@ -11,7 +11,7 @@ const Charts = () => {
         }
         
         fetchAPI();
-    });
+    }, []);
     const lineChart = (
         dailyData.length ? (
             <Line
@@ -33,9 +33,26 @@ const Charts = () => {
             />
         ) : null
     );
+    console.log(confirmed, recovered, deaths);
+    const barChart = (
+        confirmed?
+        (
+            <Bar 
+            data={{ 
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{ 
+                    label: 'People', 
+                    backgroundColor: ['rgba(0,0,255,0.5)', 'rgba(0,255,0,0.5)', 'rgba(255,0,0,0.5)'],
+                    data: [confirmed.value, recovered.value, deaths.value],
+                }],
+            }} 
+            options={{ legend: { display: false }, title: { display: true, text: `Current state in ${country}` } }}/>
+        )
+        : null
+    );
     return (
         <div className={styles.container}>
-            {lineChart}
+            { country ? barChart : lineChart }
         </div>
     )
 }
