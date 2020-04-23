434f14ca285a30d0b9fbcf7732afee6f430806e4
Nanci <sikabarca@gmail.com>
2020-04-23 00:32:27 -0500

    Show data of specific country in cards

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -9,7 +9,7 @@ const Charts = () => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
-        console.log(dailyData);
+        
         fetchAPI();
     });
     const lineChart = (
