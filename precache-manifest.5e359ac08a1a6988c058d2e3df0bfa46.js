3a012f35527127d424420ed195a4bf79a7a4bd5d
Nanci <sikabarca@gmail.com>
2020-05-27 13:06:05 -0500

    Updates

---
+++ precache-manifest.5e359ac08a1a6988c058d2e3df0bfa46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b447bb4bedeeffa0dd0504c0c9f0f1ad",
+    "url": "/corona-dashboard/index.html"
+  },
+  {
+    "revision": "e4348c4097bc829a2cc6",
+    "url": "/corona-dashboard/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "be5e447412f8a6f691e6",
+    "url": "/corona-dashboard/static/css/main.5033a461.chunk.css"
+  },
+  {
+    "revision": "e4348c4097bc829a2cc6",
+    "url": "/corona-dashboard/static/js/2.72d7c18a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-dashboard/static/js/2.72d7c18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5e447412f8a6f691e6",
+    "url": "/corona-dashboard/static/js/main.cfd82269.chunk.js"
+  },
+  {
+    "revision": "06f757c2cc0164444788",
+    "url": "/corona-dashboard/static/js/runtime-main.6640a371.js"
+  }
+]);
